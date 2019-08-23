10218de5ed5d79d9a6b5cbe2214a6b70e9f259b1
virajransing <virajransing@gmail.com>
2019-08-23 16:41:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+adding_to_dennis = "Assembly"
+programmer_hash[:dennis_ritchie][:languages].push = adding_to_dennis
+
+return programmer_hash
 
 end
